831a3727f988ed7a11f43a8cec8abc4af01e3a0c
Cassidy Crawford <cdcrawfo@ucsc.edu>
2022-04-20 13:35:32 -0700

    Lab 5
    Created a js file, and html, and a css for lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Cassidy Crawford
+* Created: 20 April 2022
+*License: public Domain
+*/
+
+//Defined Variables
+make = "Subaru";
+model = "Forester";
+color = "Silver";
+year = 2015;
+ownIt = true;
+
+//Calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
